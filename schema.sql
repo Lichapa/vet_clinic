f2fdc3df90abfcd2081a322160a64bb0b80d7ef4
Lichapa <lichapau@gmail.com>
2022-03-23 11:38:42 +0200

    Created tables vets, specialization and visits

--- schema.sql
+++ schema.sql
@@ -54,3 +54,31 @@ ADD owners_id INTEGER REFERENCES owners(id);
 -- Add column species_id which is a foreign key referencing species table
 ALTER TABLE animals
 ADD species_id INTEGER REFERENCES species(id);
+
+/* Create table vets */
+CREATE TABLE vets(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(40),
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY(id)
+);
+
+/* Created table Specialisation */
+CREATE TABLE specializations (
+    species_id INT NOT NULL,
+    vets_id INT NOT NULL,
+    FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE CASCADE,
+    FOREIGN KEY (vets_id) REFERENCES vets (id) ON DELETE CASCADE,
+    PRIMARY KEY (vets_id, species_id)
+);
+
+/* Create visits */
+CREATE TABLE visits (
+    animal_id INT NOT NULL,
+    vets_id INT NOT NULL,
+    date_of_visit DATE,    
+    FOREIGN KEY (animal_id) REFERENCES animals (id) ON DELETE CASCADE,
+    FOREIGN KEY (vets_id) REFERENCES vets (id) ON DELETE CASCADE,
+    PRIMARY KEY (vets_id, animal_id, date_of_visit)
+);
